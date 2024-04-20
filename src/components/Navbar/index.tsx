6237a812bf68d0c65f507360315dbddf19ffc52e
yommexg <hydrokidb@gmail.com>
2024-04-20 21:40:26 +0100

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,12 +8,16 @@ import MobileSidebar from "./mobileSidebar";
 
 import Logo from "../../utils/logo";
 import { scrollToTop } from "../../utils/scrollToTop";
-import { FaBell } from "react-icons/fa";
+// import { FaBell } from "react-icons/fa";
+// import { useNavigate } from "react-router-dom";
 
 const noAvatar = new URL("../../assets/noAvatar.png", import.meta.url).href;
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [showNotificationNumber, setShowNotificationNumber] = useState(false);
+
+  // const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -29,6 +33,11 @@ const Navbar: React.FC = () => {
     document.body.style.overflow = "";
   };
 
+  // const handleNotification = () => {
+  //   navigate("/notifications");
+  //   setShowNotificationNumber(false);
+  // };
+
   return (
     <div className="md:ml-[250px] py-5 md:py-8 px-4 bg-white shadow-lg">
       <div className="flex items-center justify-between md:hidden ">
@@ -38,18 +47,20 @@ const Navbar: React.FC = () => {
           scrollToTop={mobileScrollToTop}
         />
         {!isMenuOpen && <Logo />}
-        <div className="cursor-pointer flex items-center gap-8">
+        <div className="flex items-center gap-8">
           <img
             src={userData.avatar ? userData.avatar : noAvatar}
             alt={userData.username}
             className="h-10 w-10 rounded-full"
           />
-          <div className="relative">
+          {/* <div className="relative cursor-pointer" onClick={handleNotification}>
             <FaBell size={25} />
-            <p className="absolute -top-4 right-0 text-red-500 font-semibold">
-              0
-            </p>
-          </div>
+            {showNotificationNumber && (
+              <p className="absolute -top-4 right-0 text-red-500 font-semibold">
+                0
+              </p>
+            )}
+          </div> */}
         </div>
       </div>
     </div>
